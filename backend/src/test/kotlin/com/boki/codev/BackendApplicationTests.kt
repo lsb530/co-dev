366c98f7d568a01f25fa76f841ec587beb7db4a2
boki <lsb530@naver.com>
2025-03-08 21:01:14 +0900

    init project

---
+++ backend/src/test/kotlin/com/boki/codev/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boki.codev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
