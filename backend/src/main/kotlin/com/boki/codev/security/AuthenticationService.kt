bdc6f75da97192d33ffbe1523eb9c2a53b4cd204
boki <lsb530@naver.com>
2025-03-24 15:53:12 +0900

    인증 로그 추가(info)

--- backend/src/main/kotlin/com/boki/codev/security/AuthenticationService.kt
+++ backend/src/main/kotlin/com/boki/codev/security/AuthenticationService.kt
@@ -2,35 +2,23 @@ package com.boki.codev.security
 
 import com.boki.codev.redis.RefreshToken
 import com.boki.codev.redis.RefreshTokenRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 class AuthenticationService(
     private val authManager: AuthenticationManager,
-    private val userDetailsService: UserDetailsService,
     private val jwtTokenService: JwtTokenService,
     private val refreshTokenRepository: RefreshTokenRepository,
 ) {
-    fun getAuthentication(authenticationRequest: AuthenticationRequest): Authentication {
-        val authenticate = authManager.authenticate(
-            UsernamePasswordAuthenticationToken(
-                authenticationRequest.email,
-                authenticationRequest.password
-            )
-        )
-        return authenticate
-    }
-
-    fun getUserDetails(authenticationRequest: AuthenticationRequest): UserDetails {
-        return userDetailsService.loadUserByUsername(authenticationRequest.email)
-    }
-
     fun authentication(authenticationRequest: AuthenticationRequest): AuthenticationResponse {
+        logger.info { "<< ${authenticationRequest.email} login requested" }
+
         val authentication = getAuthentication(authenticationRequest)
 
         val accessToken = createAccessToken(authentication)
@@ -42,12 +30,24 @@ class AuthenticationService(
             refreshTokenRepository.delete(foundRefreshToken)
         }
 
+        logger.info { ">> ${authenticationRequest.email} login successfully" }
+
         refreshTokenRepository.save(RefreshToken(email, refreshToken))
 
         return AuthenticationResponse(
             accessToken = accessToken,
             refreshToken = refreshToken
         )
+    }
+
+    private fun getAuthentication(authenticationRequest: AuthenticationRequest): Authentication {
+        val authenticate = authManager.authenticate(
+            UsernamePasswordAuthenticationToken(
+                authenticationRequest.email,
+                authenticationRequest.password
+            )
+        )
+        return authenticate
     }
 
     private fun createAccessToken(authentication: Authentication): String {
